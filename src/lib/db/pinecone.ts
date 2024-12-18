74d89579c3130af623b0759a721f20714d0f910e
Natdanai <frame.sk140@gmail.com>
2024-12-18 08:13:38 +0700

    commit

--- src/lib/db/pinecone.ts
+++ src/lib/db/pinecone.ts
@@ -1,18 +1,19 @@
-import { Pinecone, type PineconeConfiguration } from "@pinecone-database/pinecone";
+import { Pinecone } from "@pinecone-database/pinecone";
 
 const apiKey = process.env.PINECONE_API_KEY;
+const environment = process.env.PINECONE_ENVIRONMENT;
 
 if (!apiKey) {
   throw new Error("PINECONE_API_KEY is not defined");
 }
 
-const config: PineconeConfiguration = {
+if (!environment) {
+  throw new Error("PINECONE_ENVIRONMENT is not defined");
+}
+
+const pinecone = new Pinecone({
   apiKey: apiKey,
-};
+});
 
-// const pinecone = new Pinecone({
-//   apiKey: apiKey 
-// });
-const pinecone = new Pinecone(config);
-
+// Access your index
 export const notesIndex = pinecone.Index("nextjs-ai-note-app");
